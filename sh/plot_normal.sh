e3264c5daf31488d3936c47501c4884bcb8642c7
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-01-18 17:22:34 +0100

    ugly hack

--- sh/plot_normal.sh
+++ sh/plot_normal.sh
@@ -23,15 +23,15 @@ title=$(grep Title: ${filename} | sed 's|.*Title:\(.*\)|\1|g')
 # determine, does the datafile contain two or more columns?
 NUM_COLS=$(awk 'BEGIN {FS=" "} END {print NF}' $filename)
 
-if [ $NUM_COLS -eq 3 ];
-then
-  m4 -DNAME=${name} -DFILE=${filename} \
-  -DTITLE="${title}" -DOUTPUTPATH=${OUTPUTPATH} \
-  -DFORMAT="${FORMAT}" -DEXTENSION="${EXTENSION}" gnuplot/multiplot_simple.gpi | gnuplot
-elif [ $NUM_COLS -eq 8 ]
+if [ $NUM_COLS -ge 6 ]
 then
   m4 -DNAME=${name} -DFILE=${filename} \
   -DTITLE="${title}" -DOUTPUTPATH=${OUTPUTPATH} \
   -DFORMAT="${FORMAT}" -DEXTENSION="${EXTENSION}" gnuplot/multiplot.gpi | gnuplot
+elif [ $NUM_COLS -ge 3 ];
+then
+  m4 -DNAME=${name} -DFILE=${filename} \
+  -DTITLE="${title}" -DOUTPUTPATH=${OUTPUTPATH} \
+  -DFORMAT="${FORMAT}" -DEXTENSION="${EXTENSION}" gnuplot/multiplot_simple.gpi | gnuplot
 fi
 
