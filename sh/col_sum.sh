aa065a9dc0c6d984bb75620f8d17aef7a72f84a2
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-01-18 18:18:21 +0100

    calc column sum

---
+++ sh/col_sum.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# calculate the mean of a column
+#
+
+if [ $# -eq 2 ]
+then
+FILENAME=$1
+COL=$2
+
+# else: read from stdin
+else
+  COL=$1
+fi
+
+SUM=0
+INPUTLENGTH=0
+cat $FILENAME > tmp
+for val in $(awk '{print $ARGV[2];}' tmp $COL)
+do
+  SUM=$(echo "$SUM + $val" | bc)
+done
+
+echo $SUM
+
