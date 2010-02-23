e7fe5b06dd57d3efffc855576b950e167d63a4ec
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-02-23 19:42:48 +0100

    small bugfix, now ignoring download errors

--- src/noaa.py
+++ src/noaa.py
@@ -4,6 +4,8 @@ import main
 import os
 import sys
 import ftplib
+# time: used to sleep
+import time
 
 #noaa_url="ftp://ftp.ncdc.noaa.gov/pub/data/gsod/"
 
@@ -13,7 +15,7 @@ noaa_url="205.167.25.101"
 
 def example ():
   no = NOAA ()
-  no.station_number = 998405
+  no.station_number = 37735
   no.use_wban = True
   no.getCountryList ()
   no.downloadData ()
@@ -33,9 +35,9 @@ class NOAA (Plugin):
     # createDataModel - read station data from ish-history
     self.data = data.Data (str(self.station_number), (0,0))
     # download data to tmp/noaa using ftp
-    # TODO use right time range
     files = []
-    for year in range (2009, 2011):
+    # TODO use right time range
+    for year in range (1969, 2011):
       # should look like this:
       # decide wether to use WBAN or USAF
       # 998234-99999-2010.op.gz
@@ -74,6 +76,11 @@ class NOAA (Plugin):
     for f in missing_files:
       filename = os.path.basename (f)
       print "Downloading %s........" %filename
-      ftp.retrbinary('RETR %s' %f,
-          open('cache/noaa/%s' %filename, 'w+').write)
+      try:
+        ftp.retrbinary('RETR %s' %f,
+            open('cache/noaa/%s' %filename, 'w+').write)
+      except:
+        print "%s doesn't exist." %filename
+      print "waiting for 2 sec...."
+      time.sleep (2)
     ftp.quit ()
