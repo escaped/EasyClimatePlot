b93690c5e2aa849277cf8d2906f5421dd5923dd6
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-09 17:33:53 +0100

    comment and style

--- src/weatherstation.py
+++ src/weatherstation.py
@@ -12,13 +12,14 @@ class WeatherStation (object):
   lon = ""
   elev = ""
 
-  # ctor gets one line of ish-history.txt
+  # ctor parses one line of ish-history.txt
+  # TODO das geht sicher mit weniger code
   def __init__(self, line):
-    self.usaf         = line[int (fields["USAF"][0]):int(fields["USAF"][1])]
-    self.wban         = line[int (fields["WBAN"][0]):int(fields["WBAN"][1])]
+    self.usaf         = line[int (fields["USAF"][0])        :int(fields["USAF"][1])]
+    self.wban         = line[int (fields["WBAN"][0])        :int(fields["WBAN"][1])]
     self.station_name = line[int (fields["STATION NAME"][0]):int(fields["STATION NAME"][1])]
-    self.ctry_wmo     = line[int (fields["CTRY WMO"][0]):int(fields["CTRY WMO"][1])]
-    self.ctry_fips    = line[int (fields["CTRY FIPS"][0]):int(fields["CTRY FIPS"][1])]
-    self.lat          = line[int (fields["LAT"][0]):int(fields["LAT"][1])]
-    self.lon          = line[int (fields["LON"][0]):int(fields["LON"][1])]
-    self.elev         = line[int (fields["ELEV"][0]):int(fields["ELEV"][1])]
+    self.ctry_wmo     = line[int (fields["CTRY WMO"][0])    :int(fields["CTRY WMO"][1])]
+    self.ctry_fips    = line[int (fields["CTRY FIPS"][0])   :int(fields["CTRY FIPS"][1])]
+    self.lat          = line[int (fields["LAT"][0])         :int(fields["LAT"][1])]
+    self.lon          = line[int (fields["LON"][0])         :int(fields["LON"][1])]
+    self.elev         = line[int (fields["ELEV"][0])        :int(fields["ELEV"][1])]
