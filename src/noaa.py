dec6bc4765212cfc372fca8c8bffb844d7cf8434
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-16 09:06:20 +0100

    global variables

--- src/noaa.py
+++ src/noaa.py
@@ -15,6 +15,12 @@ import sys
 import time
 import weatherstation
 
+###################
+# global variables
+###################
+STATION_LIST_CACHE_FILENAME = "stationlist"
+CACHEDIR = "cache"
+
 
 #noaa_url="ftp://ftp.ncdc.noaa.gov/pub/data/gsod/"
 # XXX
@@ -52,7 +58,7 @@ def searchStations ():
 
 def fileExistsInCache (f):
   f = os.path.basename (f)
-  return not os.path.exists (os.path.join("cache","noaa", "%s" %f))
+  return not os.path.exists (os.path.join (CACHEDIR ,"noaa", "%s" %f))
 
 class NOAA (Plugin):
   '''Objects of this class are used to access the NOAA FTP, search and download station
@@ -88,7 +94,7 @@ class NOAA (Plugin):
     for file in files:
       print "  loading data from %s" %(file)
       try:
-        f = gzip.open(os.path.join("cache","noaa", "%s" %os.path.basename (file)), "rb")
+        f = gzip.open(os.path.join(CACHEDIR,"noaa", "%s" %os.path.basename (file)), "rb")
         lines.extend(f.readlines()[1:]) # ignore first line
         f.close()
       except:
@@ -190,9 +196,9 @@ class NOAA (Plugin):
         print "Downloading %s........" %filename
         try:
           ftp.retrbinary('RETR %s' %f,
-              open(os.path.join ("cache", "noaa", "%s" %filename), 'w+').write)
+              open(os.path.join (CACHEDIR, "noaa", "%s" %filename), 'w+').write)
         except IOError:
-          print "Missing directory cache/noaa"
+          print "Missing directory %s/noaa" %CACHEDIR
           sys.exit (-1)
         except:
           print "%s doesn't exist." %filename
@@ -206,17 +212,25 @@ class NOAA (Plugin):
     # TODO we should cache the resulting list
     '''This method parses ish-history.txt, creates a WeatherStation object for each line
     and returns a list of all available WeatherStations contained in ish-history.txt.'''
-    # get ish-history
-    self.getCountryList ()
+    if not os.path.exists (os.path.join (CACHEDIR, STATION_LIST_CACHE_FILENAME)):
+      # get ish-history
+      self.getCountryList ()
 
-    # read ish-history
-    file = open (os.path.join("cache","noaa", "ish-history.txt"), 'r')
-    content = file.readlines ()
-    file.close ()
-    # delete unneeded lines
-    del content[0:20]
+      # read ish-history
+      file = open (os.path.join(CACHEDIR ,"noaa", "ish-history.txt"), 'r')
+      content = file.readlines ()
+      file.close ()
+      # delete unneeded lines
+      del content[0:20]
 
-    stations = [weatherstation.WeatherStation (line) for line in content]
+      stations = [weatherstation.WeatherStation (line) for line in content]
+      f = open (os.path.join (CACHEDIR, STATION_LIST_CACHE_FILENAME), "w")
+      cPickle.pickle (f, stations)
+      f.close ()
+    else:
+      f = open (os.path.join (CACHEDIR, STATION_LIST_CACHE_FILENAME), "r")
+      stations = cPickle.load (f)
+      f.close ()
     
     return stations
 
