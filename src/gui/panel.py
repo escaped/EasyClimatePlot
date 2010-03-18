49b0c550e28383a7e1ac1ca4fa6c1e57ee133d6e
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-18 16:15:51 +0100

    noaa example not needed anymore

--- src/gui/panel.py
+++ src/gui/panel.py
@@ -4,46 +4,6 @@
 
 import wx
 
-class NOAA_Panel (wx.Panel):
-  '''The NOAA panel can dynamically switch it's subpanels.'''
-  def __init__ (self, parent):
-    self.parent = parent
-    wx.Panel.__init__ (self, parent)
-
-    # initialize panels
-    self.pan1 = Panel1 (self)
-    self.pan1.Show (True)
-    self.pan2 = Panel2 (self)
-    self.pan2.Show (False)
-
-    self.current_panel = self.pan1
-    
-    # grid
-    self.grid = wx.GridBagSizer (hgap = 2, vgap = 1)
-    self.button = wx.Button (self, label = "please click me...", pos=(20,20))
-    self.main = Panel1 (self)
-    self.grid.Add (self.button, pos = (0,0))
-    self.grid.Add (self.current_panel, pos = (0,1))
-    self.Bind (wx.EVT_BUTTON, self.onClick, self.button)
-
-    self.SetSizerAndFit (self.grid)
-
-
-  def switch (self, panel):
-    # switch current_panel to panel
-    self.grid.Remove (self.current_panel)
-    self.current_panel.Show (False)
-    self.grid.Add (panel, pos = (0,1))
-    self.current_panel = panel
-    self.current_panel.Show (True)
-
-  def onClick (self, e):
-    self.switch (self.pan2)
-    self.grid.Remove (self.button)
-    self.button.Destroy ()
-    self.grid.Layout ()
-
-
 class Panel1 (wx.Panel):
   def __init__ (self, parent):
     wx.Panel.__init__ (self, parent)
