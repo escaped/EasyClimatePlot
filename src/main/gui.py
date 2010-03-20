e3b65f4b12f4c6f1da2f7e2b89d28ddc9ae353e0
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-20 18:05:31 +0100

    more

---
+++ src/main/gui.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import wx
+
+from noaa.gui import NOAA_Wizard
+from plot.gui import PlotIntro
+
+def ShowMainWindow ():
+  app = wx.App (False)
+  mw  = MainWindow ()
+  app.MainLoop ()
+
+class MainWindow (wx.Frame):
+  def __init__ (self):
+    wx.Frame.__init__(self, None, title='Klimadaten')
+
+
+    ######
+    # menu bar
+    ######
+    menuBar = wx.MenuBar ()
+
+    # menus
+    programMenu = wx.Menu ()
+    menuClose = programMenu.Append (wx.ID_CLOSE, "&Schließen", " Programm schließen")
+
+    helpMenu = wx.Menu ()
+    menuAbout = helpMenu.Append (wx.ID_ABOUT, "&About", " Über dieses Programm")
+
+    # append menus to menubar
+    menuBar.Append (programMenu, "&Programm")
+    menuBar.Append (helpMenu, "&Hilfe")
+
+    # callbacks
+    self.Bind (wx.EVT_MENU, self.onClose, menuClose)
+
+    self.Bind (wx.EVT_MENU, self.onAbout, menuAbout)
+
+    # finish menubar construction
+    self.SetMenuBar (menuBar)
+    
+    self.notebook = wx.Notebook (self)
+    # das hinzufügen sollte dynamisch vollzogen werden
+
+    # TODO anders machen
+    workflow = NOAA_Wizard (self.notebook)
+    self.notebook.AddPage (workflow, "Download")
+    workflow.Create ()
+
+    plot = PlotIntro (self.notebook)
+    self.notebook.AddPage (plot , "Plot")
+    self.notebook.AddPage (EmptyPanel (self.notebook), "Export")
+
+    self.Show ()
+
+  def onClose (self, e):
+    '''Close the frame'''
+    self.Close (True)
+
+  def onAbout (self, e):
+    '''Show the about message box'''
+    # TODO hier sollte etwas sinnvolles stehen
+    mbox = wx.MessageDialog (self, "Ein einfaches Programm.", "Über das Progamm.", wx.OK)
+    mbox.ShowModal ()
+    mbox.Destroy ()
+
+class EmptyPanel (wx.Panel):
+  def __init__ (self, *args, **kwargs):
+    wx.Panel.__init__ (self, *args, **kwargs)
+
