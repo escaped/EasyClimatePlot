8946f7f1ce9511341e28c5c8a3cf1fa0caf0657a
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-23 18:02:52 +0100

    da fehlt noch was

---
+++ src/export/gui.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from mvc.workflow.hook import Hook
+from mvc.workflow.viewcontrol import ViewControl
+from mvc.workflow.wizard import Wizard
+
+
+import export.noaa.batchgeocode as enb
+
+import wx
+
+class ExportIntro (ViewControl):
+  def createSubPanels (self):
+    self.pool.addWindow ("ChooseType", PlotType (self))
+    self.pool.addWindow ("NOAA_Wizard", enb.NOAA (self))
+
+class PlotType (Hook, wx.Panel):
+  def __init__ (self, *args, **kwargs):
+    wx.Panel.__init__ (self, *args, **kwargs)
+    self.parent = args[0]
+    
+    # self.sizer
+    self.sizer = wx.BoxSizer (wx.VERTICAL)
+
+    # three buttons (hehe..)
+    self.noaa  = wx.Button (self, -1, label="NOAA Stationsdaten")
+
+    self.sizer.Add (self.noaa)
+
+    self.SetSizer (self.sizer)
+    #self.Bind (wx.EVT_BUTTON, self.onWalterLieth, self.walterlieth)
+
