7b504f6697803f7a06688154f58bbc4c2666336b
Alexander Frenzel <alex@relatedworks.com>
2010-03-23 22:39:52 +0100

    moved noaa

--- src/plugins/noaa/__init__.py
+++ src/plugins/noaa/__init__.py
@@ -1,17 +1,18 @@
 from pluginmanager import WizardPlugin
+import gui
 
 class NOAA(WizardPlugin):
   def getName(self):
     return "NOAA"
-  
+
   def getDescription(self):
     return ""
-  
+
   def getAuthor(self):
     return "Magnus Mueller, Alexander Frenzel"
-    
+ 
   def getType(self):
     return WizardPlugin.TYPE["input"]
-  
-  def getWizard(self):
-    raise Exception("Not implemented.")
+
+  def getWizard(self, parent):
+    return gui.NOAA_Wizard(parent)
