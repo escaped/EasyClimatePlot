81e982cc486e2d0629d8e8c4ebbd9ea5bec0c318
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-23 22:26:33 +0100

    more on mvc

---
+++ src/plot/walterlieth/control.py
@@ -0,0 +1,13 @@
+from mvc.control import Control
+
+class SearchControl (Control):
+  def __init__ (self, view):
+    Control.__init__ (self, view)
+
+class SearchResultsControl (Control):
+  def __init__ (self, view):
+    Control.__init__ (self, view)
+
+class PlotControl (Control):
+  def __init__ (self, view):
+    Control.__init__ (self, view)
