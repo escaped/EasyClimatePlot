eb038ed7b331c8166250fe87868a7e19e4e27316
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-03-24 20:31:09 +0100

    einmal was maximiertes!

--- src/wxcustom/notebook.py
+++ src/wxcustom/notebook.py
@@ -22,15 +22,26 @@ class Notebook (wx.Notebook):
     except KeyError:
       self.categories[category] = []
       self.categoryPages[category] = CategoryPage (self)
-      self.AddPage (self.categoryPages[category], str (category))
+      self.AddPage (self.categoryPages[category], str (category), wx.EXPAND)
 
       self.categories[category].append (item)
-      self.categoryPages[category].listctrl.InsertStringItem (str(item))
+      self.categoryPages[category].insertString (str(item))
+
 
 class CategoryPage (wx.Panel):
   def __init__ (self, *args, **kwargs):
     wx.Panel.__init__ (self, *args, **kwargs)
+    self.sizer = wx.BoxSizer ()
     self.listctrl = wx.ListCtrl (self)
+    # TODO diese sizer sind eine komische sache..
+    # mit dem unteren aufruf wird das zumindest vertikal expandiert
+    self.sizer.Add (self.listctrl, -1, wx.EXPAND,0)
+
+    self.SetSizerAndFit (self.sizer)
+    self.SetAutoLayout (True)
+
+  def insertString(self, string):
+    self.listctrl.InsertStringItem (wx.ID_NEW, string)
 
 # usage test
 
@@ -40,15 +51,15 @@ if __name__ == "__main__":
   class TestPanel (wx.Panel):
     def __init__ (self, parent):
       wx.Panel.__init__ (self, parent)
-      self.txt = wx.TextCtrl(self, -1, "hallo")
+      self.txt = wx.TextCtrl(self, -1, "hallo", style=wx.EXPAND)
   
   app   = wx.App (False)
   frame = wx.Frame (None)
   nb    = Notebook (frame)
 
   testpanel = TestPanel (nb)
-
   nb.addItem ("hallo", testpanel)
+
 
   frame.Show ()
   app.MainLoop ()
