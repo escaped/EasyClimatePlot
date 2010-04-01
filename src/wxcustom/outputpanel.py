2d6a134e8af97722ac759f84e715af12e663e89e
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-04-01 23:39:58 +0200

    clear output

--- src/wxcustom/outputpanel.py
+++ src/wxcustom/outputpanel.py
@@ -9,15 +9,25 @@ class OutputPanel (Panel):
     Panel.__init__ (self, *args, **kwargs)
     self.parent = args[0]
 
-    self.sizer = wx.BoxSizer () 
+    self.sizer = wx.BoxSizer (wx.VERTICAL) 
     self.textctrl = outputctrl.OutputCtrl(self, style=wx.TE_MULTILINE|wx.TE_AUTO_SCROLL)
     self.sizer.Add (self.textctrl, -1, wx.EXPAND, 0)
+
+    # clear button
+
+    self.btnClear = wx.Button (self, 0, label="Ausgabe leeren")
+    self.sizer.Add (self.btnClear)
+    self.Bind (wx.EVT_BUTTON, self.onClear, self.btnClear)
+
     self.SetSizer (self.sizer)
     self.Layout ()
 
   def write (self, string):
     self.textctrl.write (string)
     self.parent.Show (True)
+
+  def onClear (self, evt):
+    self.textctrl.Clear ()
 
 
 # test cases
